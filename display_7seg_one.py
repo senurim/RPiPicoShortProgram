c9153a07102e23af7a46fd68f17f582f55029f65
Seonwoo Lee <senurim@gmail.com>
2022-04-29 15:05:59 +0900

    FND 제어를 위한 문서 작성함

--- display_7seg_one.py
+++ display_7seg_one.py
@@ -1,75 +1,14 @@
-# 1st program to display a 4xFND
+# FND 함수를 이용하여 각 자리수를 바꿔가며 0~F까지 표시하는 프로그램
 #
-# try to control each segment on the target dig
+# @author = senu
 
-from machine import Pin
+import fndControl as fnd
 import time
-
-#define output pins and FND elements
-dig1 = Pin(13, Pin.OUT, value=1)  #initially turn off
-dig2 = Pin(12, Pin.OUT, value=1)
-dig3 = Pin(11, Pin.OUT, value=1)
-dig4 = Pin(10, Pin.OUT, value=1)
-
-digitLst = [dig1, dig2, dig3, dig4]
-
-segA=Pin(9,Pin.OUT)
-segB=Pin(8,Pin.OUT)
-segC=Pin(7,Pin.OUT)
-segD=Pin(6,Pin.OUT)
-segE=Pin(18,Pin.OUT)
-segF=Pin(19,Pin.OUT)
-segG=Pin(20,Pin.OUT)
-segDP=Pin(21,Pin.OUT)
-
-segLst=[segA, segB, segC, segD, segE, segF, segG]
-
-# 0~9 mapping table
-segmap = {0:[1,1,1,1,1,1,0], 1:[0,1,1,0,0,0,0], 2:[1,1,0,1,1,0,1],\
-          3:[1,1,1,1,0,0,1], 4:[0,1,1,0,0,1,1], 5:[1,0,1,1,0,1,1],\
-          6:[1,0,1,1,1,1,1], 7:[1,1,1,0,0,0,0], 8:[1,1,1,1,1,1,1],\
-          9:[1,1,1,1,0,1,1], 10:[1,1,1,0,1,1,1], 11:[0,0,1,1,1,1,1],\
-          12:[1,0,0,1,1,1,0], 13:[0,1,1,1,1,0,1], 14:[1,0,0,1,1,1,1],\
-          15:[1,0,0,0,1,1,1]}
-
-#define a function to display one number on 7segments unit
-def displayNumber(num):
-    '''
-    Display one number at one 7 segment
-    parameter: num - 0 ~ 15 integer number to be displayed
-    '''
-    if num >= 0 and num < 16: #check the valid range
-        mapinfo = segmap[num]
-        for i in range(7):
-            segLst[i].value(mapinfo[i])
-    else:
-        mapinfo = segmap[14]
-        for i in range(7):
-            segLst[i].value(mapinfo[i])
-
-def displayDigitOne(digit, num):
-    '''
-    a function to display one number at the given digit
-    -parameter: digit - 0 ~ 3 (left to right), num - 0 ~ 15 
-    '''
-    if digit >= 0 and digit <=3 :
-        digitLst[digit].value(0)      #turn on the given digit
-    else:
-        print('Err: wrong digit')
-
-    displayNumber(num)
-
-def turnoffDigit(digit):
-    '''
-    Turn off the given digit segment
-    -parameter: digit - 1 ~ 4 (left to right)
-    '''
-    digitLst[digit].value(1)
 
 digitcnt = 0
 for i in range(16):
-    displayDigitOne(digitcnt,i)
-    time.sleep(1)
-    turnoffDigit(digitcnt)
+    fnd.displayDigitOne(digitcnt,i)
+    time.sleep(0.7)
+    fnd.turnoffDigit(digitcnt)
     digitcnt += 1
     digitcnt %= 4
---
+++ fndControl.py
@@ -0,0 +1,69 @@
+# FND 제어를 위한 여러 함수를 모듈 방식으로 재편성함.
+# module name: fndControl.py
+#
+from machine import Pin
+import time
+
+#define output pins and FND elements
+dig1 = Pin(13, Pin.OUT, value=1)  #initially turn off
+dig2 = Pin(12, Pin.OUT, value=1)
+dig3 = Pin(11, Pin.OUT, value=1)
+dig4 = Pin(10, Pin.OUT, value=1)
+
+digitLst = [dig1, dig2, dig3, dig4]
+
+segA=Pin(9,Pin.OUT)
+segB=Pin(8,Pin.OUT)
+segC=Pin(7,Pin.OUT)
+segD=Pin(6,Pin.OUT)
+segE=Pin(18,Pin.OUT)
+segF=Pin(19,Pin.OUT)
+segG=Pin(20,Pin.OUT)
+segDP=Pin(21,Pin.OUT)
+
+segLst=[segA, segB, segC, segD, segE, segF, segG]
+
+# 0~9 mapping table
+segmap = {0:[1,1,1,1,1,1,0], 1:[0,1,1,0,0,0,0], 2:[1,1,0,1,1,0,1],\
+          3:[1,1,1,1,0,0,1], 4:[0,1,1,0,0,1,1], 5:[1,0,1,1,0,1,1],\
+          6:[1,0,1,1,1,1,1], 7:[1,1,1,0,0,0,0], 8:[1,1,1,1,1,1,1],\
+          9:[1,1,1,1,0,1,1], 10:[1,1,1,0,1,1,1], 11:[0,0,1,1,1,1,1],\
+          12:[1,0,0,1,1,1,0], 13:[0,1,1,1,1,0,1], 14:[1,0,0,1,1,1,1],\
+          15:[1,0,0,0,1,1,1]}
+
+#define a function to display one number on 7segments unit
+def displayNumber(num):
+    '''
+    Display one number at one 7 segment
+    parameter: num - 0 ~ 15 integer number to be displayed
+    '''
+    if num >= 0 and num < 16: #check the valid range
+        mapinfo = segmap[num]
+        for i in range(7):
+            segLst[i].value(mapinfo[i])
+    else:
+        mapinfo = segmap[14]
+        for i in range(7):
+            segLst[i].value(mapinfo[i])
+
+def displayDigitOne(digit, num, dpOn=False):
+    '''
+    a function to display one number at the given digit
+    -parameter: digit - 0 ~ 3 (left to right), num - 0 ~ 15, dpOn: DP on/off default=False 
+    '''
+    if digit >= 0 and digit <=3 :
+        digitLst[digit].value(0)      #turn on the given digit
+    else:
+        print('Err: wrong digit')
+
+    displayNumber(num)
+
+    if dpOn:
+        segDP.on()
+
+def turnoffDigit(digit):
+    '''
+    Turn off the given digit segment
+    -parameter: digit - 1 ~ 4 (left to right)
+    '''
+    digitLst[digit].value(1)
