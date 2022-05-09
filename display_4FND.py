e3394688901c2e04922e26a0415204203b3fcc6a
Seonwoo Lee <senurim@gmail.com>
2022-05-09 14:37:21 +0900

    기초 라이브러리 모듈 사용 형태로 변경 보드 실행시 Ctrl-C 명령 작동 안함

--- display_4FND.py
+++ display_4FND.py
@@ -1,159 +1,34 @@
-# 1st program to display a 4xFND
+# 간단한 4자리 숫자를 다이나믹 구동 방법으로 표시하는 프로그램
+# 미리 관련 함수를 만들어 놓은 모듈 (fndControl.py) 이용
 #
-# try to control each segment on the target dig
+# @author=senu
 
-from machine import Pin
-from machine import Timer
-from machine import RTC
 import time
+import fndControl as fnd
 
-
-#define output pins and FND elements
-dig1 = Pin(13, Pin.OUT, value=1)  #initially turn off
-dig2 = Pin(12, Pin.OUT, value=1)
-dig3 = Pin(11, Pin.OUT, value=1)
-dig4 = Pin(10, Pin.OUT, value=1)
-
-digitLst = [dig1, dig2, dig3, dig4]
-
-segA=Pin(9,Pin.OUT)
-segB=Pin(8,Pin.OUT)
-segC=Pin(7,Pin.OUT)
-segD=Pin(6,Pin.OUT)
-segE=Pin(18,Pin.OUT)
-segF=Pin(19,Pin.OUT)
-segG=Pin(20,Pin.OUT)
-segDP=Pin(21,Pin.OUT)
-
-segLst=[segA, segB, segC, segD, segE, segF, segG]
-
-# 0~9 mapping table
-segmap = {0:[1,1,1,1,1,1,0], 1:[0,1,1,0,0,0,0], 2:[1,1,0,1,1,0,1],\
-          3:[1,1,1,1,0,0,1], 4:[0,1,1,0,0,1,1], 5:[1,0,1,1,0,1,1],\
-          6:[1,0,1,1,1,1,1], 7:[1,1,1,0,0,0,0], 8:[1,1,1,1,1,1,1],\
-          9:[1,1,1,1,0,1,1], 10:[1,1,1,0,1,1,1], 11:[0,0,1,1,1,1,1],\
-          12:[1,0,0,1,1,1,0], 13:[0,1,1,1,1,0,1], 14:[1,0,0,1,1,1,1],\
-          15:[1,0,0,0,1,1,1]}
-
-#global variables
-#display on flag
-displayOnState = False
-#time duration for dynamic driving
-displayRate = 5000  #unit micro sec, default = 5 msec
-
-
-
-#define a function to display one number on 7segments unit
-def displayNumber(num):
-    '''
-    Display one number at one 7 segment
-    parameter: num - 0 ~ 15 integer number to be displayed
-    '''
-    if num >= 0 and num < 16: #check the valid range
-        mapinfo = segmap[num]
-        for i in range(7):
-            segLst[i].value(mapinfo[i])
-    else:
-        mapinfo = segmap[14]
-        for i in range(7):
-            segLst[i].value(mapinfo[i])
-
-def displayDigitOne(digit, num):
-    '''
-    a function to display one number at the given digit
-    -parameter: digit - 0 ~ 3 (left to right), num - 0 ~ 15 
-    '''
-    if digit >= 0 and digit <=3 :
-        digitLst[digit].value(0)      #turn on the given digit
-    else:
-        print('Err: wrong digit')
-        return 
-
-    displayNumber(num)
-
-def turnoffDigit(digit):
-    '''
-    Turn off the given digit segment
-    -parameter: digit - 1 ~ 4 (left to right)
-    '''
-    digitLst[digit].value(1)
-
-def turnoff4Fnd():
-    '''
-    Turn off the 4 digits 7 segments
-    '''
-    global displayOnState
-    if displayOnState:
-        displayOnState = False
-        for i in range(4):
-            turnoffDigit(i)
-    
-def onoffDP(digit, flag):
-    '''
-    Turn on the given digit's DP (dot)
-    '''
-    if digit >= 0 and digit <=3 :
-        if flag:
-            digitLst[digit].value(0)      #turn on the given digit
-            segDP.value(1)
-        else:
-             digitLst[digit].value(1)      #turn off the given digit
-             segDP.value(0)
-    else:
-        print('Err: wrong digit')
-        return 
-    
-
-# def displayFour(numbers):
-#     '''
-#     Display four numbers at 4 X FND by using dynamic driving method
-#     parameter: numbers - tuple datatype containing 4 numbers from left to right
-#     '''
-
-#     #check the given numbers
-#     for itm in numbers:
-#         if itm < 0 or itm > 15:
-#             print('Err: given number is outof the valid range')
-#             return
-
-#     global displayOnState
-#     displayOnState = True
-
-#     digitcnt = 0
-
-#     #define a callback function
-#     def dis_fnd_callback(numbers):
-#         displayDigitOne(digitcnt, numbers[digitcnt])
-#         digitcnt += 1
-#         digitcnt %= 4
-
-# #initialize a timer for dynamic driving
-# fndtmr = Timer(period=displayRate, mode=Timer.PERIODIC, callback=dis_fnd_callbak)
 
 if __name__ == '__main__':
-    #use RTC 
-    rtc = machine.RTC()
-    rtc.datetime([2022,5,9,1,18,50,0,0])
+    # #use RTC 
+    # rtc = machine.RTC()
+    # rtc.datetime([2022,5,9,1,18,50,0,0])
     
-    #toggling 2nd seg's DP to display sec movement
+    # #toggling 2nd seg's DP to display sec movement
     
-    tmr = Timer(freq=2, mode=Timer.PERIODIC, callback=lambda t: segDP.toggle())
-    # tmr.init()
-
+    # tmr = Timer(freq=2, mode=Timer.PERIODIC, callback=lambda t: segDP.toggle())
     
+    #input the displayed number and check its range
+    disnum = int(input('Enter the 4 digits number : '))
+    while disnum > 9999 or disnum < 0:
+        disnum = int(input('Enter the 4 digits number : '))
+    
+    now_dis=[disnum//1000, ((disnum%1000)//100), ((disnum%100)//10), (disnum%10) ]
     while True:
-        nowts = rtc.datetime()
-        now_dis = [nowts[4]//10, nowts[4]%10, nowts[5]//10, nowts[5]%10 ]
-        # now_dis=[1,2,3,4]
-        for i in range(4):
-            displayDigitOne(i,now_dis[i])
-            time.sleep_us(displayRate)
-            turnoffDigit(i)
-
-    # DP 세그먼트 시험
-    # for i in range(4):
-    #     onoffDP(i, True)
-    #     time.sleep(0.5)
-    #     onoffDP(i, False)
+        # nowts = rtc.datetime()
+        # now_dis = [nowts[4]//10, nowts[4]%10, nowts[5]//10, nowts[5]%10 ]
         
+        for i in range(4):
+            fnd.displayDigitOne(i,now_dis[i])
+            time.sleep_us(fnd.displayRate)
+            fnd.turnoffDigit(i)
+
         
