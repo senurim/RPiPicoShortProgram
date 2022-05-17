26871d1bdf07deb157df8672ca0308963c1a4d8f
Seonwoo Lee <senurim@gmail.com>
2022-05-17 17:27:54 +0900

    시간설정 기능 구현

--- fullfunc_clock.py
+++ fullfunc_clock.py
@@ -3,6 +3,9 @@
 # 1차 목표: Down 스위치 누르면 날짜/시간 번갈아서 디스플레이함
 # 2차 목표: 초 표시까지
 # 3차 목표: 스위치 조작을 통한 시간 설정 기능
+#       3.1 오래 누르기 감지 기능 추가 (setting mode / running mode change)
+#            1.5초 이상 누르면 작동
+#       3.2 setting mode: time tuple (year, month, day, hour, min) 설정하기
 
 import fndControl as fnd
 from machine import Timer
@@ -10,8 +13,8 @@ from machine import RTC
 from machine import Pin
 
 #hardware configuration
-upsw = Pin(0, Pin.IN, Pin.PULL_UP)
-dnsw = Pin(1, Pin.IN, Pin.PULL_UP)
+modesw = Pin(0, Pin.IN, Pin.PULL_UP)
+setsw = Pin(1, Pin.IN, Pin.PULL_UP)
 
 #init RTC
 rtc = RTC()
@@ -21,8 +24,10 @@ currentDigit = 0  #current digit counter 0~3
 displayNumber=[0,0,0,0 ]  #default values of 4 numbers
 dotFlag=[False,True,False,False]  #DP segment ON/OFF info for each digit
 disMode=0  #display mode variable 0:display time, 1: date MM.DD, 2: seconds __SS
-setMode=False   #time setting flag variable , if True, then time setting mdoe
-
+longSwCnt = 0 # long time switch pressing counter that is increased in 0.5sec IRQ handler
+setMode = False   #time setting flag variable , if True, then time setting mdoe
+cTime=(2022,5,17,2,12,0,0,0)
+blinkFlag = True
 
 #use two different timers
 # first: 0.5sec timer -- toggling 2nd digit DP seg.
@@ -50,42 +55,124 @@ def halfsec_cb(halfsectmr):
     global dotFlag
     global displayNumber
     global disMode
+    global setMode
+    global longSwCnt
+    global cTime
+    global blinkFlag
 
-    if disMode == 0 : # mode 0; displya HH:MM
-        cTime = rtc.datetime()
-        displayNumber=[cTime[4]//10, cTime[4]%10, cTime[5]//10, cTime[5]%10]
-        dotFlag[3] = False
-        dotFlag[1] = not dotFlag[1]
+    cTime = rtc.datetime()
 
-    elif disMode == 1: #mode 1: mm:dd
-        cTime = rtc.datetime()
-        displayNumber=[cTime[1]//10, cTime[1]%10, cTime[2]//10, cTime[2]%10]
-        dotFlag[1] = dotFlag[3] = True
+    # handling for longSwCnt 
+    if longSwCnt != 0:  #press set button once
+        #read the set sw value
+        if setsw.value() == 0:  #pressed ?
+            longSwCnt += 1
+        else:
+            longSwCnt = 0   #reset the counter
+
     
-    elif disMode == 2: #mode 2: __:ss
-        cTime = rtc.datetime()
-        displayNumber=[16,16, cTime[6]//10, cTime[6]%10]
-        dotFlag[1] = not dotFlag[1]
-        dotFlag[3] = not dotFlag[3]
-
+    if longSwCnt == 4: #more than 1.5sec then change the mode btwn set/display
+        longSwCnt = 0
+        if not setMode:
+            setMode = True
+            disMode = 0
+        else:
+            setMode = False
+            disMode = 0
+    
+    # handling display for two modes
+    # 
+    if not setMode: # In normal mode
+        if disMode == 0 : # mode 0; displya HH:MM
+            displayNumber=[cTime[4]//10, cTime[4]%10, cTime[5]//10, cTime[5]%10]
+            dotFlag= [False, not dotFlag[1], False, False]
+            
+        elif disMode == 1: #mode 1: mm:dd
+            displayNumber=[cTime[1]//10, cTime[1]%10, cTime[2]//10, cTime[2]%10]
+            dotFlag=[ False, True, False, True]
         
-
-def upSWcb(upsw):
+        elif disMode == 2: #mode 2: __:ss
+            displayNumber=[16,16, cTime[6]//10, cTime[6]%10]
+            dotFlag = [ False, not dotFlag[1], False, not dotFlag[3]]
+        
+    else: # In setting mode
+        if disMode == 0 : # mode 0; displya H.H._ _
+            displayNumber=[cTime[4]//10, cTime[4]%10, 16, 16 ]
+            dotFlag=[True, True, False, False]
+        
+        elif disMode == 1 : # mode 1; displya _ _M.M.
+            displayNumber=[16, 16, cTime[5]//10, cTime[5]%10 ]
+            dotFlag=[False, False, True, True]
+        
+        elif disMode == 2 : # mode 2; displya mm._ _
+            displayNumber=[cTime[1]//10, cTime[1]%10, 16, 16 ]
+            dotFlag=[False, True, False, False]
+        elif disMode == 3 : # mode 3; displya _ _.dd
+            displayNumber=[16, 16, cTime[2]//10, cTime[2]%10 ]
+            dotFlag=[False, True, False, False]
+        
+        
+def modeSWcb(modesw):
     '''
-    upSWcb() function: up switch callback function
-    This functions called when the user pushes the UP switch. Then change the display mode (increase value)
+    modeSWcb() function: mode switch callback function
+    This function handle the mode switch clicking events:
+      Normal mode: pushing this switch to change the display mode
+      Setting mode: increase the value of each digits
     '''
     global disMode
-    disMode += 1
-    disMode %= 3    #first try
+    global setMode
+    global cTime
 
-def downSWcb(dnsw):
+    if not setMode:
+        disMode += 1
+        disMode %= 3    
+    else: # in setting mode increase the value
+        newTS = list(cTime)
+        if disMode == 0: #increase HH
+            newTS[4] += 1
+            if newTS[4] > 23:
+                newTS[4] = 0
+        elif disMode == 1: #increase MM
+            newTS[5] += 1
+            if newTS[5] > 59:
+                newTS[5] = 0
+        elif disMode == 2: #increase mm
+            newTS[1] += 1
+            if newTS[1] > 12:
+                newTS[1] = 1
+        elif disMode == 3: #increase mm
+            newTS[2] += 1
+            if newTS[2] > 31:
+                newTS[2] = 1    
+
+        #set up rtc
+        rtc.datetime(newTS)
+
+
+
+def setDownSWcb(setsw):
+    '''
+    setDownSwcb() function: setting switch callback function
+    The role of thi CB func: start to count the longSwCnt variable
+    '''
+    # global setMode
+    global longSwCnt
     global disMode
+    global setMode
+
+    
+    longSwCnt = 1
+    
+    if setMode:
+        disMode += 1
+        disMode %= 4  # set mode: 0 ~ 3
+
+
     
 
 # initialize the IRQs of Pins
-upsw.irq(upSWcb, trigger=Pin.IRQ_FALLING )
-dnsw.irq(downSWcb, trigger=Pin.IRQ_FALLING )
+modesw.irq(modeSWcb, trigger=Pin.IRQ_FALLING )
+setsw.irq(setDownSWcb, trigger=Pin.IRQ_FALLING )
 
 
 #run timers
